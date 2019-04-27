f8bf8b55b1f46439246b95457b264bfbc7a970d2
Talha Balaj <talhabalaj@hotmail.com>
2019-04-27 13:00:28 +0500

    added url as argv[1] if provided

--- main.cpp
+++ main.cpp
@@ -2,8 +2,16 @@
 #include "Request/Request.h"
 using namespace std;
 
-int main() {
-  Request req("google.com");
+int main(int argc, char **argv)
+{
+  string url;
+
+  if (argc < 2)
+    url = "google.com";
+  else
+    url = argv[1];
+
+  Request req(url);
   Response res = req.getResponse();
   cout << res.content();
   cout << res.code();
